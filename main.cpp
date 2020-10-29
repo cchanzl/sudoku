be116c3ef75a3a0edc86f785e12e74e95fd57c36
Zhan Chan <zc620@vm-shell2.doc.ic.ac.uk>
2020-10-29 15:07:38 +0000

    added credits for execution timer

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main() {
       
   /* This section illustrates the use of the pre-supplied helper functions. */
   cout << "============= Pre-supplied functions =============" << "\n\n";
-    
+      
   cout << "Calling load_board():" << '\n';
   load_board("easy.dat", board);
 
@@ -51,7 +51,7 @@ int main() {
   // write more tests
   
   cout << "=================== Question 3 ===================" << "\n\n";
-
+  
   load_board("easy.dat", board);
 
   if (save_board("easy-copy.dat", board)) {
@@ -62,7 +62,7 @@ int main() {
   cout << '\n';
   
   cout << "=================== Question 4 ===================" << "\n\n";
-
+  
   load_board("easy.dat", board);
   
   if (solve_board(board)) {
@@ -103,7 +103,8 @@ int main() {
     cout << "A solution cannot be found." << '\n';
   }
   cout << '\n';
-  
+
+  //Credit: Code for execution timer is taken from the example in  http://www.cplusplus.com/reference/chrono/high_resolution_clock/now/
   //high_resolution_clock::time_point t1 = high_resolution_clock::now();
   
   load_board("mystery3.dat", board);
